c5a55ab9ca5bc6978e117278cdf1a356d481e1ae
pontakornth <pontakorn61@gmail.com>
2020-05-25 20:06:07 +0700

    refactor: Change impl to File

--- src/algorithm/deflate.rs
+++ src/algorithm/deflate.rs
@@ -2,15 +2,16 @@ use flate2::write::{DeflateEncoder, DeflateDecoder};
 use flate2::Compression;
 use std::io;
 use std::io::prelude::*;
+use std::fs::File;
 
-pub fn compress(src: &mut impl Read,target: impl Write) -> io::Result<()> {
+pub fn compress(src: &mut File,target: File) -> io::Result<()> {
     let mut data: Vec<u8> = vec![];
     src.read_to_end(&mut data)?;
     let mut compressor = DeflateEncoder::new(target, Compression::default());
     compressor.write_all(&data)
 }
 
-pub fn decompress(src: &mut impl Read, target: impl Write) -> io::Result<()> {
+pub fn decompress(src: &mut File, target: File) -> io::Result<()> {
     let mut data: Vec<u8> = vec![];
     src.read_to_end(&mut data)?;
     let mut decompressor = DeflateDecoder::new(target);
